339331e70827a75d0ce3e2a7c49b8a0f4e034475
Austen Collins <austencollins@gmail.com>
2015-12-10 19:39:26 -0800

    change to be a boilerplate project

--- index.js
+++ index.js
@@ -1,55 +1,62 @@
 'use strict';
 
-const BbPromise = require('bluebird');
+module.exports = function(root) {
 
-/**
- * ServerlessOptimizer
- */
-
-class ServerlessOptimizer {
+  const path     = require('path'),
+      SPlugin    = require(path.join(root, 'ServerlessPlugin')),
+      BbPromise  = require('bluebird');
 
   /**
-   * Constructor
+   * ServerlessOptimizer
    */
 
-  constructor(S, config) {
-    super(S, config);
+  class ServerlessOptimizer extends SPlugin {
+
+    /**
+     * Constructor
+     */
+
+    constructor(S, config) {
+      super(S, config);
+    }
+
+    /**
+     * Define your plugins name
+     */
+
+    static getName() {
+      return 'com.serverless.' + ServerlessOptimizer.name;
+    }
+
+    /**
+     * Register Hooks
+     */
+
+    registerHooks() {
+
+      this.S.addHook(this._hook.bind(this), {
+        action: 'functionRunLambdaNodeJsPost',
+        event:  'post'
+      });
+
+      return Promise.resolve();
+    }
+
+    _hook(evt) {
+
+      let _this = this;
+
+      return new BbPromise(function (resolve, reject) {
+
+        console.log("----------- HOOK RUN");
+
+        return resolve(evt);
+
+      });
+    }
   }
 
-  /**
-   * Define your plugins name
-   */
+  // Export Class
+  return ServerlessOptimizer;
 
-  static getName() {
-    return 'com.yourdomain.' + ServerlessOptimizer.name;
-  }
-
-  /**
-   * Register Hooks
-   */
-
-  registerHooks() {
-
-    this.S.addHook(this._hook.bind(this), {
-      action: 'codePackageLambdaNodejs',
-      event:  'post'
-    });
-
-    return Promise.resolve();
-  }
-
-  _hook(evt) {
-
-    let _this = this;
-
-    return new BbPromise(function (resolve, reject) {
-
-      console.log("HOOK RUN");
-
-      return resolve(evt);
-
-    });
-  }
-}
-
-module.exports = ServerlessOptimizer;
+};
